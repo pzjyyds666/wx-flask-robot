2de20e9a15926cb40f779ffc96975f2fcfed8313
pzjyyds666 <103363503+pzjyyds666@users.noreply.github.com>
2023-05-23 23:38:12 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from wxcloudrun import app
 
 # 启动Flask Web服务
 if __name__ == '__main__':
-    app.run(host=sys.argv[1], port=sys.argv[2])
+    app.run(host=sys.argv[1], port=80)
     # app.run(port='8080', debug=True)
