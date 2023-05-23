38cb3d8d437730b536576b28b8bae1a0804a2ebc
pzjyyds666 <2817295942@qq.com>
2023-05-23 22:28:13 +0800

    repair src and werobot pkg

--- run.py
+++ run.py
@@ -6,3 +6,4 @@ from wxcloudrun import app
 # 启动Flask Web服务
 if __name__ == '__main__':
     app.run(host=sys.argv[1], port=sys.argv[2])
+    # app.run(port='8080', debug=True)
--- wxcloudrun/robot.py
+++ wxcloudrun/robot.py
@@ -1,9 +1,13 @@
-from werobot import WeRoBot
-
+import werobot
 print('load robot.py')
 
-robot = WeRoBot(
-    token='xxxxxx',# 对应公众号的token设置
+robot = werobot.WeRoBot(
+    token='test_token',# 对应公众号的token设置
 #     encoding_aes_key='xxxxxx',# 明文传输不需要填写
 #     app_id='xxxxx'#明文传输不需要填写
 )
+
+
+@robot.handler
+def handel_reply(message):
+    return 'received'
